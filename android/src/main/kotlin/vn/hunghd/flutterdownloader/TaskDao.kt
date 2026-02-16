ad75556639d502b88e54e97ed47c3f80ec545bdc
mmirio <87669391+mmirio@users.noreply.github.com>
2026-02-16 17:39:14 +0200

    test8

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDao.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDao.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
+import android.util.Log
 
 class TaskDao(private val dbHelper: TaskDbHelper) {
     private val projection = arrayOf(
