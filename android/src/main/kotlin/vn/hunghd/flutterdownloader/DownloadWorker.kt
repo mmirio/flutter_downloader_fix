3795b4946115fc8469aea3767fbc5fef1dfdce8e
mmirio <87669391+mmirio@users.noreply.github.com>
2026-02-16 17:31:40 +0200

    test6=7

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
@@ -452,7 +452,7 @@ class DownloadWorker(context: Context, params: WorkerParameters) :
                         )
                         if (intent != null) {
                             log("Setting an intent to open the file $savedFilePath")
-                            taskDao!!.updateTaskPath(id.toString(),downloadedFilePath)
+                            taskDao!!.updateTaskPath(id.toString(),savedFilePath)
 
                             val flags: Int =
                                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE else PendingIntent.FLAG_CANCEL_CURRENT
