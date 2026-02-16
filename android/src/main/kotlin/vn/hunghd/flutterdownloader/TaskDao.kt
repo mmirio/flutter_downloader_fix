9a73d36d7f83d1f34f10c160c9636540480bcc63
mmirio <87669391+mmirio@users.noreply.github.com>
2026-02-16 18:02:12 +0200

    test10

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDao.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDao.kt
@@ -15,6 +15,7 @@ class TaskDao(private val dbHelper: TaskDbHelper) {
         TaskEntry.COLUMN_NAME_URL,
         TaskEntry.COLUMN_NAME_FILE_NAME,
         TaskEntry.COLUMN_NAME_SAVED_DIR,
+        TaskEntry.COLUMN_NAME_FILE_PATH,
         TaskEntry.COLUMN_NAME_HEADERS,
         TaskEntry.COLUMN_NAME_MIME_TYPE,
         TaskEntry.COLUMN_NAME_RESUMABLE,
